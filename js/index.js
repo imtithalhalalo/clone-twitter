6b624a7af8fdfdd8d187b066e3d52a2b6ce58b29
imtithalhalalo <halaloimtithal@gmail.com>
2022-09-15 11:30:16 +0300

    dob dropdown fill using js

--- js/index.js
+++ js/index.js
@@ -19,3 +19,24 @@ const closePopupSignUp = () => {
 }
 
 close_sign_up.addEventListener('click', closePopupSignUp)
+
+var month_box = document.getElementById('month_box')
+var days_box = document.getElementById('days_box')
+var years_box = document.getElementById('years_box')
+
+//months dropdown
+var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+for (var i = 0; i < month.length; i++){
+    month_box.options.add(new Option(month[i]))
+}
+
+//days dropdown
+for (var i = 1; i <= 31; i++){
+    days_box.options.add(new Option(i))
+} 
+
+//years dropdown
+for (var i = 2022; i >= 1902 ; i--){
+    years_box.options.add(new Option(i))
+} 
