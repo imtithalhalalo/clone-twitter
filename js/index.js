d6da358f60b0a2ea4162a1480d80c3626ec979fd
imtithalhalalo <halaloimtithal@gmail.com>
2022-09-15 11:51:10 +0300

    js sign in pop up close and open

--- js/index.js
+++ js/index.js
@@ -1,24 +1,42 @@
 const popup_sign_up = this.document.getElementById('pop-sign-up')
+const popup_sign_in = this.document.getElementById('pop-sign-in')
 
 const open_sign_up = this.document.getElementById('open-sign-up')
+const open_sign_in = this.document.getElementById('open-sign-in')
 
 const close_sign_up = this.document.getElementById('close-sign-up')
+const close_sign_in = this.document.getElementById('close-sign-in')
 
 const container = this.document.getElementById('con')
+
+// open popup sign up & sign in
 const openPopupSignUp = () => {
     popup_sign_up.classList.add('open-popup')
     container.classList.add('grey-fade-over')
 }
 
+const openPopupSignIn = () => {
+    popup_sign_in.classList.add('open-popup')
+    container.classList.add('grey-fade-over')
+}
 open_sign_up.addEventListener('click', openPopupSignUp)
+open_sign_in .addEventListener('click', openPopupSignIn)
 
 
+//close popup sign up & sign in
 const closePopupSignUp = () => {
         popup_sign_up.classList.remove('open-popup')
         container.classList.remove('grey-fade-over')
 }
 
+const closePopupSignIn = () => {
+    popup_sign_in.classList.remove('open-popup')
+    container.classList.remove('grey-fade-over')
+}
+
 close_sign_up.addEventListener('click', closePopupSignUp)
+close_sign_in.addEventListener('click', closePopupSignIn)
+
 
 var month_box = document.getElementById('month_box')
 var days_box = document.getElementById('days_box')
