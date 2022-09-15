62dcf220ebbc984161f1099a5d98bf1295e68aaf
imtithalhalalo <halaloimtithal@gmail.com>
2022-09-15 21:26:10 +0300

    Adding getMonth getDay getYear functions js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -38,15 +38,15 @@ close_sign_up.addEventListener('click', closePopupSignUp)
 close_sign_in.addEventListener('click', closePopupSignIn)
 
 
-const month_box = document.getElementById('month')
-const days_box = document.getElementById('days')
-const years_box = document.getElementById('years')
+const month_box = document.getElementById('month_box')
+const days_box = document.getElementById('days_box')
+const years_box = document.getElementById('years_box')
 
 //months dropdown
 const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
-for (const i of month){
-    month_box.options.add(new Option(month[i]))
+for (let i of month){
+    month_box.options.add(new Option(i))
 }
 
 //days dropdown
@@ -58,3 +58,18 @@ for (let i = 1; i <= 31; i++){
 for (let i = 2022; i >= 1902 ; i--){
     years_box.options.add(new Option(i))
 } 
+
+let getMonth = () => {
+    var value = month_box.options[month_box.selectedIndex].value
+    return value
+}
+
+const getDay = () => {
+    var value = days_box.options[days_box.selectedIndex].value
+    return value
+}
+
+const getYear = () => {
+    var value = years_box.options[years_box.selectedIndex].value
+    return value
+}
